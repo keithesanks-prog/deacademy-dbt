8121bd381f22f452225357a0c7e5e5704f5a6e94
KEITH SANKS <KSANKSTEMP@gmail.com>
2026-01-12 20:38:28 +0000

    added descriptions

--- tests/assert_stg_stripe_payment_total_positive.sql
+++ tests/assert_stg_stripe_payment_total_positive.sql
@@ -1,5 +1,5 @@
-select order_id
-where sum(amount) as total_amount
+select order_id,
+    sum(amount) as total_amount
 from {{ref("stg_stripe__payments")}}
-grouby 1
-having sum(amount) < 0
+group by 1
+having total_amount < 0
