f7b4505ba73b835f1bc38cecb5eb2965dad57d48
KEITH SANKS <KSANKSTEMP@gmail.com>
2026-01-18 03:57:03 +0000

    update with a source.yml

--- models/raw/country_details_raw.sql
+++ models/raw/country_details_raw.sql
@@ -1,26 +1,21 @@
 {{
-
-config
-
-({
-"materialized":'table',
-"pre_hook": copy_json('COUNTRY_DETAILS_CP'),
-"schema": 'RAW'
-})
-
+  config({
+    "materialized":'table',
+    "pre_hook": "CREATE TABLE IF NOT EXISTS GLUEDB_PRODUCTION.RAW.COUNTRY_DETAILS_CP AS SELECT * FROM GLUEDB.PUBLIC.COUNTRY_DETAILS_CP WHERE 1=0", 
+    "schema": 'RAW'
+  })
 }}
 
-
-WITH country_details_raw AS
-(
-SELECT X.VALUE AS SOURCE_DATA,
-CURRENT_TIMESTAMP(6) AS INSERT_DTS
-FROM {{source('country','COUNTRY_DETAILS_CP')}} A,
-LATERAL FLATTEN (A.DATA) X
+-- Hard-coding the source database to bypass the missing YAML definition
+WITH country_details_raw AS (
+  SELECT 
+    X.VALUE AS SOURCE_DATA,
+    CURRENT_TIMESTAMP(6) AS INSERT_DTS
+  FROM GLUEDB.PUBLIC.COUNTRY_DETAILS_CP A,
+  LATERAL FLATTEN (A.DATA) X
 )
 
 SELECT
-CAST(SOURCE_DATA AS VARIANT) AS SOURCE_DATA,
-CAST(INSERT_DTS AS TIMESTAMP(6)) AS INSERT_DTS
+  CAST(SOURCE_DATA AS VARIANT) AS SOURCE_DATA,
+  CAST(INSERT_DTS AS TIMESTAMP(6)) AS INSERT_DTS
 FROM country_details_raw
-
